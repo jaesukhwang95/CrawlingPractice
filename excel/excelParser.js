5300a99995344c5ac017aebfc857c255313d0acd
HwangJaeSuk <jaesukhwang95@gmail.com>
2019-05-30 11:35:05 +0900

    sync html parser

--- excel/excelParser.js
+++ excel/excelParser.js
@@ -13,7 +13,7 @@ for(const [i, r] of records.entries()) {
 }
 
 const crawler = async () => {
-    await Promise.all(records.map( async (r) => {
+    for (const [i, r] of records.entries()) {
         const response = await axios.get(r.링크);
         if (response.status === 200) {  
             const html = response.data;
@@ -21,6 +21,20 @@ const crawler = async () => {
             const text = $('.score.score_left .star_score').text();
             console.log(r.제목, '평점', text.trim());
         }
-    }));
+    }
 };
+
+// const crawler = async () => {
+//     await Promise.all(records.map( async (r) => {
+//         const response = await axios.get(r.링크);
+//         if (response.status === 200) {  
+//             const html = response.data;
+//             const $ = cheerio.load(html);
+//             const text = $('.score.score_left .star_score').text();
+//             console.log(r.제목, '평점', text.trim());
+//         }
+//     }));
+// };
+// 순서 보장하지 않음
+
 crawler();
